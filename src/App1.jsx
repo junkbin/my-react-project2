13e7a2df2fc5d29b199edcd60925f90172fb7dee
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-11-26 18:07:14 +0530

    react.

---
+++ src/App1.jsx
@@ -0,0 +1,24 @@
+import Assets from "./Assets";
+import Assets1 from "./Assets1";
+import Assets2 from "./Assets2";
+
+// JSX - Javascript and XML
+// <App />
+// User Defined Tag
+// Component.
+function App1() {
+  const p1 =
+    "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ratione minima facere minus vel? Dignissimos aliquam ducimus facilis provident mollitia accusantium molestias! Suscipit at, enim quos excepturi aliquid molestiae aperiam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum ratione minima facere minus vel? Dignissimos aliquam ducimus facilis provident mollitia accusantium molestias! Suscipit at, enim quos excepturi aliquid molestiae aperiam!";
+  return (
+    <div>
+      <Assets />
+      <hr />
+      <Assets1 title="Mumbai" />
+      <hr />
+      <Assets2 title="Calcutta" paragraph={p1} />
+      <Assets2 title="Chennai" paragraph={p1} />
+    </div>
+  );
+}
+
+export default App1;
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <div className="bg-dark text-light h1 p-3">Header</div>
+    </div>
+  );
+}
+
+export default Header;
